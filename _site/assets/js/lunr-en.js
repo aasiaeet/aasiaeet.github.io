c12474e6afad6a6f17a2179e38924d17861b1d0c
aasiaeet <amir.asiaeetaheri@gmail.com>
2018-01-04 01:38:26 -0500

    bibliography

--- _site/assets/js/lunr-en.js
+++ _site/assets/js/lunr-en.js
@@ -38,7 +38,7 @@ var idx = lunr(function () {
     
       this.add({
           title: "Applied Machine Learning",
-          excerpt: "\n",
+          excerpt: "“Hello”\n",
           categories: [],
           tags: [],
           id: 3
@@ -83,7 +83,7 @@ var idx = lunr(function () {
     
       this.add({
           title: "Theoretical Machine Learning",
-          excerpt: "\n",
+          excerpt: "“Dear”\n",
           categories: [],
           tags: [],
           id: 8
@@ -680,7 +680,7 @@ var store = [
       {
         "title": "Applied Machine Learning",
         "url": "http://localhost:4000/courses/aml/info/",
-        "excerpt": "\n",
+        "excerpt": "“Hello”\n",
         "teaser":
           
             null
@@ -735,7 +735,7 @@ var store = [
       {
         "title": "Theoretical Machine Learning",
         "url": "http://localhost:4000/courses/tml/info",
-        "excerpt": "\n",
+        "excerpt": "“Dear”\n",
         "teaser":
           
             null
